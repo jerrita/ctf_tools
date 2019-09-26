1b887bc3865e595333a9302c9f1ff42570a5b168
jerrita <782981223@qq.com>
2019-09-26 18:19:11 +0800

    url_tool v1.1完成

---
+++ exinstall.py
@@ -0,0 +1,4 @@
+import os
+print('正在安装对应拓展...')
+os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple requests')
+os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pyqt5==5.10.1')
---
+++ url_tool/main.py
@@ -0,0 +1,50 @@
+import sys
+
+# if hasattr(sys, 'frozen'):
+#     os.environ['PATH'] = sys._MEIPASS + ";" + os.environ['PATH']
+from PyQt5.QtCore import QUrl
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from gui import *
+import requests
+
+
+class MyWidget(QWidget, Ui_Form):
+    def __init__(self, parent=None):
+        super(MyWidget, self).__init__(parent)
+        self.setupUi(self)
+
+    def go_clicked(self):
+        get = self.getEdit.toPlainText()
+        post = self.postEdit.toPlainText()
+        head = self.headEdit.toPlainText()
+        url = self.urlEdit.text() + '?'
+        for i in get.split():
+            url += i
+            if i != get.split()[-1]:
+                url += '&'
+        data = {}
+        for i in post.split():
+            t = i.split('=')
+            data[t[0]] = t[1]
+        header = {}
+        for i in head.split():
+            t = i.split('=')
+            header[t[0]] = t[1]
+        res = requests.post(url, data, headers=header)
+        res.encoding = 'utf-8'
+        self.sourceShow.setPlainText(res.text)
+        view = QWebEngineView(self.browView)
+        view.setHtml(res.text)
+        title = res.text.split('<title>')[1].split('</title>')[0]
+        print(title)
+        self.titleShow.setText(title)
+        view.setFixedWidth(self.browView.width())
+        view.setFixedHeight(self.browView.height())
+        view.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    myWin = MyWidget()
+    myWin.show()
+    sys.exit(app.exec_())
